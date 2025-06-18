4733c10416f8219006bb314b6dbea7d4a47c6100
Csaszar Daniel <csaszardaniel81@gmail.com>
2025-06-18 21:39:02 +0200

    made a story screen

--- main.js
+++ main.js
@@ -105,19 +105,12 @@ function add(a, b) {
 				font: "20px Courier",
 				fill: "#00ffcc",
 			});
-			if (!scene.textures.exists("winBlock")) {
-				const winGfx = scene.add.graphics();
-				winGfx.fillStyle(0xffff00, 1);
-				winGfx.fillRect(0, 0, 50, 50);
-				winGfx.generateTexture("winBlock", 50, 50);
-				winGfx.destroy();
-			}
 
 			const level = levels[scene.levelIndex];
 			const winBlock = scene.physics.add.staticSprite(
 				level.winBlock.x,
 				level.winBlock.y,
-				"winBlock"
+				"data"
 			);
 
 			// Add an overlap check. When player touches it, call puzzleSolved.
@@ -145,6 +138,68 @@ function add(a, b) {
 	// TODO add more levels
 ];
 
+class StoryScene extends Phaser.Scene {
+	constructor() {
+		super("StoryScene");
+	}
+
+	preload() {
+		this.load.image("story-bg", "/assets/story_background.png");
+	}
+
+	create() {
+		this.add
+			.image(size.width / 2, size.height / 2, "story-bg")
+			.setDisplaySize(size.width, size.height);
+
+		const storyLines = [
+			"You are Zero, a programmer whose virtual world has been corrupted by a rival hacker, Nyx.",
+			"The world glitches, its fundamental laws breaking down.",
+			"You must dive into the code, solve the puzzles, and purge the corruption.",
+			"",
+			"Click anywhere to begin...",
+		];
+		const fullStoryText = storyLines.join("\n");
+
+		const textObject = this.add.text(60, (size.height / 4) * 3, "", {
+			font: "32px Courier",
+			fill: "#000000",
+			align: "center",
+			backgroundColor: "#dcdcdc",
+			padding: { x: 20, y: 20 },
+			wordWrap: { width: size.width - 200 },
+		});
+
+		this.isTyping = true;
+		let charIndex = 0;
+		const timedEvent = this.time.addEvent({
+			delay: 40,
+			callback: () => {
+				textObject.text += fullStoryText[charIndex];
+				charIndex++;
+				if (charIndex >= fullStoryText.length) {
+					this.isTyping = false;
+					timedEvent.remove();
+				}
+			},
+			repeat: fullStoryText.length - 1,
+		});
+		this.input.on("pointerdown", () => {
+			if (!this.isTyping) {
+				timedEvent.remove();
+				textObject.text = fullStoryText;
+				this.isTyping = false;
+			} else {
+				this.scene.start("MainScene", { levelIndex: 0 });
+			}
+		});
+
+		this.input.once("pointerdown", () => {
+			this.scene.start("MainScene", { levelIndex: 0 });
+		});
+	}
+}
+
 class MainScene extends Phaser.Scene {
 	constructor() {
 		super("MainScene");
@@ -159,6 +214,7 @@ class MainScene extends Phaser.Scene {
 	preload() {
 		this.load.image("player", "/assets/player.png");
 		this.load.image("terminal", "/assets/terminal.png");
+		this.load.image("data", "/assets/data.png");
 	}
 
 	create() {
@@ -375,7 +431,7 @@ class MainScene extends Phaser.Scene {
 	}
 }
 
-function startGame(levelIndex = 0) {
+function startGame() {
 	const gameContainer = document.getElementById("game-container");
 	if (gameContainer) {
 		gameContainer.innerHTML = "";
@@ -395,7 +451,7 @@ function startGame(levelIndex = 0) {
 			height: size.height,
 			autoCenter: Phaser.Scale.CENTER_BOTH,
 		},
-		scene: [new MainScene(levelIndex)],
+		scene: [StoryScene, MainScene],
 	};
 	new Phaser.Game(config);
 }
